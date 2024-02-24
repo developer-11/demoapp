8443db7515458e6223be92e2c1093ad5d5b6a3c3
arsalaan kh <arsalaanahmed1998@gmail.com>
2024-02-25 00:20:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const user_cont = document.querySelector('.user-container');
+const regis_btn = document.querySelector('.registration-btn');
+const login_btn = document.querySelector('.login-btn');
+
+
+regis_btn.addEventListener('click',()=>{
+    user_cont.classList.add('login-section--display')
+})
+
+login_btn.addEventListener('click',()=>{
+    user_cont.classList.remove('login-section--display')
+})
